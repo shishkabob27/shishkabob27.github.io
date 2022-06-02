f6bb6c6598c60333bab0170701332d704c0414e4
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-06-02 17:52:41 -0500

    Screenshots / logos

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const gameContentHolder = document.getElementById('gameContentHolder')
 const gameContentClose = document.getElementById('gameContentClose')
+const gameContentTitle = document.getElementById('gameContentTitle')
 
-function viewGameContent() {
+function viewGameContent(game) {
     gameContentHolder.hidden = false;
+
+    switch (game) {
+        case 'bulletsiteretold':
+            gameContentTitle.src = "/files/images/games/bulletsiteretold/logo_br.png"
+            break;
+        case 'sgc':
+            gameContentTitle.src = "/files/images/games/sgc/sgc_logo.png"
+            break;
+        case 'bulletsite':
+            gameContentTitle.src = "/files/images/games/bulletsite/Title_banner.png"
+            break;
+        case 'dontfreeze':
+            gameContentTitle.src = "/files/images/games/dontfreeze/logo_dt.png"
+            break;
+        default:
+            break;
+    }
 }
 
 function closeGameContent() {
